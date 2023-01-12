46d80e60bd47c079dc25e2ecadf2e7749e9df0e2
RaoulBock <anthonyraoulbock01@gmail.com>
2023-01-12 14:49:59 +0200

    front-end

---
+++ Components/Cards/BotCard.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const BotCard = () => {
+  return (
+    <View>
+      <Text>BotCard</Text>
+    </View>
+  );
+};
+
+export default BotCard;
+
+const styles = StyleSheet.create({});
---
+++ Components/Cards/UserCard.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const UserCard = () => {
+  return (
+    <View>
+      <Text>UserCard</Text>
+    </View>
+  );
+};
+
+export default UserCard;
+
+const styles = StyleSheet.create({});
